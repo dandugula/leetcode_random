df31a1a7bcd9359e9016639d03f768fb2e8122bc
Chaitanya Dandugula <chaitanya@chadan.in>
2019-02-02 21:31:42 +0530

    Modified the previous solution to use lambdas

--- find_replace_pattern.cpp
+++ find_replace_pattern.cpp
@@ -2,32 +2,28 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
       std::vector<string> res;
-
-      for(auto aWord: words) {
+      std::for_each(words.begin(), words.end(), 
+      [&res, &pattern](auto& aWord) {
         std::vector<int> letterMap(26, -1);
         std::vector<bool> letterRec(26, false);
         int i = 0;
         bool match = true;
+        std::for_each(pattern.begin(), pattern.end(), 
+        [&aWord, &letterMap, &letterRec, &i, &match] (auto& aLetter) {
+            if(letterMap.at(aLetter - 'a') == -1 && !letterRec.at(aWord[i] - 'a')) {
+                letterMap[aLetter - 'a'] = aWord[i];
+                letterRec[aWord[i] - 'a'] = true;
 
-        for(auto aLetter: pattern) {
-          if(letterMap.at(aLetter - 'a') == -1 && !letterRec.at(aWord[i] - 'a')) {
+            }else if(letterMap.at(aLetter - 'a') != aWord[i]) {
+              match = false;
+              return;
+            }
 
-            letterMap[aLetter - 'a'] = aWord[i];
-            letterRec[aWord[i] - 'a'] = true;
-          }else if(letterMap.at(aLetter - 'a') != aWord[i]) {
-
-            match = false;
-            break;
-          }
-
-          if(!match) break;
-          
-          ++i;
-        }
+            ++i;
+        });
         if(match)
           res.push_back(aWord);
-      }
-
+      });
       return res;
     }
 };
