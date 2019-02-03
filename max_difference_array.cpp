6d539d5998e1c2fe7c2df5df81d39c5cfab2c024
Chaitanya Dandugula <chaitanya@chadan.in>
2019-02-03 10:16:35 +0530

    Fixed solution to Cisco coding challenge - 1 problem.

--- max_difference_array.cpp
+++ max_difference_array.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <algorithm>
 
 int max_difference(const std::vector<int>& v) {
   int max_diff = -1;
-  int max_num = v[0];
-  for(int i = 1; i < v.size(); ++i) {
-    if(max_num < v[i]) {
-      max_diff = max_diff != -1 ? max_diff + v[i] - max_num : v[i] - max_num;
-      max_num = v[i];
-    }
-  }
+  int min_num = *v.begin();
+  std::for_each(v.begin() + 1, v.end(), [&max_diff, &min_num](auto& aNum) {
+                if(aNum < min_num)
+                  min_num = aNum;
+                else
+                  max_diff = std::max(max_diff, aNum - min_num);
+               });
   return max_diff;
 }
 
@@ -22,14 +23,17 @@ int main () {
   std::vector<int> case5 = {2, 10, 1, 2, 3, 22};
   std::vector<int> case6 = {-2, -10, -1, -2, -3, -22};
   std::vector<int> case7 = {10, 5, 11};
+  std::vector<int> case8 = {2, 10, 5, 2, 3, 22};
 
   std::cout << "Running Tests..." << std::endl;
   assert (max_difference(case1) == 1);
   assert (max_difference(case2) == -1);
   assert (max_difference(case3) == 2);
   assert (max_difference(case4) == 8);
-  assert (max_difference(case5) == 20);
+  assert (max_difference(case5) == 21);
+  assert (max_difference(case6) == 9);
   assert (max_difference(case7) == 6);
+  assert (max_difference(case8) == 20);
   std::cout << "All Tests passed!!" << std::endl;
 
   return 0;
