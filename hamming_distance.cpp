d827813ab8a0058e9beed9ad1bba70762df21178
Chaitanya Dandugula <chaitanya@chadan.in>
2019-02-24 11:47:04 +0530

    +4 leetcode solutions

---
+++ hamming_distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  int hammingDistance(int x, int y) {
+    int res = 0;
+
+    while(x || y) {
+      res += ((x & 0x0001) != (y & 0x0001));
+      x >>= 1;
+      y >>= 1;
+    }
+
+    return res;
+  }
+};
+
