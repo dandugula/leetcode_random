67ebc95dd6d4f9a2e199835d97775266bc26015c
Chaitanya Dandugula <chaitanya@chadan.in>
2019-04-16 20:08:28 +0530

    Squezed some memory and optimized run time of 3 leetcode solutions

--- town_judge.cpp
+++ town_judge.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     int findJudge(int N, vector<vector<int>>& trust) {
-        std::vector<std::vector<int>> trust_matrix(N, std::vector<int>(N, 0));
-        int ret = -1;
-        bool bflag = false;
+        std::vector<std::vector<bool>> trust_matrix(N, std::vector<bool>(N, false));
+        std::vector<bool> zero_rows (N, true);
 
-        for(auto aPair : trust) {
-            trust_matrix[aPair[0]-1][aPair[1]-1] = 1;
+        int zero_row = -1;
+        int cur_row;
+
+        for(auto aPair : trust) { 
+            trust_matrix[aPair[0]-1][aPair[1]-1] = true;
+            zero_rows[aPair[0]-1] = false;
         }
-
-        for(int i = 0; i < N; ++i) {
-            bflag = false;
-
-            for(int j = 0; j < N; ++j)
-                if(trust_matrix[i][j] == 1) {
-                    bflag = true;
-                    break;
-                }
-
-            if(bflag) continue;
-            bflag = false;
-
-            for(int j = 0; j < N; ++j)
-                if(trust_matrix[j][i] == 0 && i != j) {
-                    bflag = true;
-                    break;
-                }
-
-            if(bflag) continue;
-            else {
-                ret = i + 1;
+        
+        for(int i = 0; i < zero_rows.size(); ++i) {
+            if( zero_rows[i] && -1 == zero_row) {
+                zero_row = i;
+            } else if( zero_rows[i] && -1 != zero_row) {
+                zero_row = -1;
                 break;
             }
-
         }
-                
-        return ret;
+    
+        for(cur_row = 0; -1 != zero_row && cur_row < trust_matrix.size(); 
+                ++cur_row) {
+
+            if(zero_row == cur_row) continue;
+
+            if(!trust_matrix[cur_row][zero_row]) break;
+        }
+
+        return -1 == zero_row || cur_row != trust_matrix.size()
+               ? -1 : zero_row + 1;
     }
 };
